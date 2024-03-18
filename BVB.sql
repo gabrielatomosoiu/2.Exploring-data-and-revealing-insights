6f08b837442d0fd7a79e69ddfab9b9501ae64399
Gabriela <136575175+gabrielatomosoiu@users.noreply.github.com>
2024-03-18 16:17:04 +0200

    Update BVB.sql

--- BVB.sql
+++ BVB.sql
@@ -81,8 +81,8 @@ ORDER BY Industry_Sector, Ticker, Year;
 --	CMP		Compa ( CMP )                                          	Consumer Discretionary	2023	0.4388
 --	EFO		Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2022	0.2030
 --	EFO		Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2023	0.2562
---	SFG		Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2022	14.0000
---	SFG		Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2023	15.6037
+--	SFG		Sphera Franchise Group ( SFG )	                       Consumer Discretionary	2022	14.0000
+--	SFG		Sphera Franchise Group ( SFG )	                       Consumer Discretionary	2023	15.6037
 
 --       ATB       	Antibiotice ( ATB )	                                   Consumer Staples	       2022	0.5660
 --       ATB       	Antibiotice ( ATB )	                                   Consumer Staples	       2023	0.5799
