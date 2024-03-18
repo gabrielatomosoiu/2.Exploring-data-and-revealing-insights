1ed1d00ab871cf61126fd87ea958a22130aa85de
Gabriela <136575175+gabrielatomosoiu@users.noreply.github.com>
2024-03-18 16:14:01 +0200

    Update BVB.sql

--- BVB.sql
+++ BVB.sql
@@ -73,16 +73,16 @@ ORDER BY Industry_Sector, Ticker, Year;
 -----------------------------------------------------------------------------------------------------------------------------
 --       Result:
 
---       Ticker	Company_Name	                                          Industry_Sector	       Year	AverageClosingPrice
+--	Ticker		Company_Name	                                          Industry_Sector	       Year	AverageClosingPrice
 
---       DIGI	       Digi Communications ( DIGI )	                     Communication Services	2022	31.5000
---       DIGI        Digi Communications ( DIGI )	                     Communication Services	2023	34.0276
---       CMP       	Compa ( CMP )	                                          Consumer Discretionary	2022	0.3920
---       CMP       	Compa ( CMP )                                          	Consumer Discretionary	2023	0.4388
---       EFO	       Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2022	0.2030
---       EFO	       Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2023	0.2562
---       SFG       	Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2022	14.0000
---       SFG       	Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2023	15.6037
+--	DIGI			Digi Communications ( DIGI )	                     Communication Services	2022	31.5000
+--	DIGI			Digi Communications ( DIGI )	                     Communication Services	2023	34.0276
+--	CMP			Compa ( CMP )	                                          Consumer Discretionary	2022	0.3920
+--	CMP			Compa ( CMP )                                          	Consumer Discretionary	2023	0.4388
+--	EFO			Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2022	0.2030
+--       EFO       Turism, Hoteluri, Restaurante Marea Neagra ( EFO )	Consumer Discretionary	2023	0.2562
+--       SFG       Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2022	14.0000
+--       SFG       Sphera Franchise Group ( SFG )	                     Consumer Discretionary	2023	15.6037
 
 --       ATB       	Antibiotice ( ATB )	                                   Consumer Staples	       2022	0.5660
 --       ATB       	Antibiotice ( ATB )	                                   Consumer Staples	       2023	0.5799
